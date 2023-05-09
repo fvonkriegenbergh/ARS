ef3a1f9551fe5cf8c00d5dd1fd2bc32e40124ced
fabianvonk@gmail.com <fabianvonk@gmail.com>
2023-05-09 13:35:36 -0700

    Added Flights and My Flights pages

---
+++ src/BookingForm.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class BookingForm {
+    private JTable flightsTable;
+    private JPanel rootPanel;
+    private JButton customerSupportBtn;
+    private JButton backButton;
+    private JLabel flightNumLabel;
+    private JLabel departDestLabel;
+    private JLabel arriveDestLabel;
+    private JLabel departTimeLabel;
+    private JLabel arriveTimeLabel;
+    private JLabel baggageLabel;
+    private JLabel seatNumLabel;
+    private JLabel ticketNumLabel;
+    private JButton bookButton;
+
+}
---
+++ src/FlightsForm.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class FlightsForm {
+
+
+    private JPanel rootPanel;
+    private JLabel flightNumLabel;
+    private JLabel departDestLabel;
+    private JLabel arriveDestLabel;
+    private JLabel departTimeLabel;
+    private JLabel arriveTimeLabel;
+    private JLabel baggageLabel;
+    private JLabel seatNumLabel;
+    private JLabel ticketNumLabel;
+    private JButton bookButton;
+    private JTable flightsTable;
+    private JButton customerSupportBtn;
+    private JButton backButton;
+}
