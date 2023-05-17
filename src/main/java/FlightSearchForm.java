f0ad0b77a613459e16b1bef706ee9b45639e2f44
Ahmed <ali068@csusm.edu>
2023-05-16 21:40:49 -0700

    Sign-up and Login page working as intended

---
+++ src/main/java/FlightSearchForm.java
@@ -0,0 +1,82 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class FlightSearchForm extends JFrame implements ActionListener {
+    private JPanel mainSearchPan;
+    private JLabel departDate;
+    private JPanel departArrivalLocationPan;
+    private JPanel departArrivalDatePan;
+    private JTextField departLocInput;
+    private JLabel departureLocationLbl;
+    private JLabel arrivalLocLbl;
+    private JTextField arrivalLocationInput;
+    private JTextField departDateInput;
+    private JTextField returnDateInput;
+    private JLabel returnDateILbl;
+    private JButton searchFlightBtn;
+    private JButton searchBackBtn;
+    private User currUser ;
+
+    // default constructor
+    public FlightSearchForm(){
+
+        this.searchFlightBtn.addActionListener(this) ;
+        this.searchBackBtn.addActionListener(this) ;
+
+        this.setTitle("Search Flight");
+        this.setContentPane(this.mainSearchPan);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE) ;
+        this.setSize(600, 400);
+        this.setLocationRelativeTo(null) ;
+        this.setVisible(true);
+    }
+
+    // constructor that takes in user in parameter
+    public FlightSearchForm(User currUser){
+
+        this.currUser = currUser ;
+
+        this.searchFlightBtn.addActionListener(this) ;
+        this.searchBackBtn.addActionListener(this) ;
+
+        this.setTitle("Search Flight");
+        this.setContentPane(this.mainSearchPan);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE) ;
+        this.setSize(600, 300);
+        this.setLocationRelativeTo(null) ;
+        this.setVisible(true);
+    }
+
+    // override actionListener for searchFlightBtn
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == searchFlightBtn){
+            String departLocInput = this.departLocInput.getText() ;
+            String arrivalLocInput = this.arrivalLocationInput.getText() ;
+            String departDateInput = this.departDate.getText() ;
+            String returnDateInput = this.returnDateInput.getText() ;
+
+            if((departLocInput.equals("")) || (arrivalLocInput.equals("")) || (departDateInput.equals("")) || (returnDateInput.equals(""))){
+                JOptionPane.showMessageDialog(this, "One or more fields missing input!");
+            }
+            else{
+                System.out.println("test Showing available flights") ;
+                searchFlights(departLocInput, arrivalLocInput, departDateInput, returnDateInput) ;
+            }
+        }
+        if(e.getSource() == searchBackBtn){
+            DashboardForm newDash = new DashboardForm(this.currUser) ;
+            this.dispose() ;
+        }
+    }
+
+    // take in user input for desired searched flight
+    // send input ot database for search query and return desired flights
+    private void searchFlights(String departLocInput, String arrivalLocInput, String departDateInput, String returnDateInput){
+
+
+        FlightsForm flightForm = new FlightsForm();
+    }
+
+}
