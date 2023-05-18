7419b60242c1cec34a1f9cf2eafcb74815220f37
CharlesDNav <naver001@csusm.edu>
2023-05-18 00:03:25 -0700

    Added display functionality for BookingForm

--- src/main/java/BookingForm.java
+++ src/main/java/BookingForm.java
@@ -3,6 +3,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
 
 public class BookingForm extends JFrame implements ActionListener, MouseListener {
     private JScrollPane scrollFlight;
@@ -20,6 +21,8 @@ public class BookingForm extends JFrame implements ActionListener, MouseListener
     private JButton bookButton;
     private JTable flightsTable;
     private User currUser;
+    private ArrayList<Flight> displayFlight ;
+    private int currClickFlight ;
 
     // default constructor
     public BookingForm(){
@@ -61,6 +64,34 @@ public class BookingForm extends JFrame implements ActionListener, MouseListener
         this.setVisible(true);
     }
 
+    // Constructor that takes in user as parameter
+    public BookingForm(User currUser, ArrayList<Flight> newFlights){
+        // set title of frame
+        this.setTitle("Available Flights") ;
+
+        // set UI display look consistent with login page
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel"); //Windows Look and feel
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            System.out.println(e.toString());
+        }
+
+        this.currUser = currUser ;
+        this.displayFlight = newFlights ;
+        this.backButton.addActionListener(this);
+        this.customerSupportBtn.addActionListener(this);
+        this.bookButton.addActionListener(this) ;
+        this.flightsTable.addMouseListener(this) ;
+
+        displayAvailableFlights(displayFlightData(this.displayFlight));
+
+        this.setContentPane(this.rootPanel);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE) ;
+        this.setSize(700, 500);
+        this.setLocationRelativeTo(null) ;
+        this.setVisible(true);
+    }
+
     @Override
     public void actionPerformed(ActionEvent e){
         if(e.getSource() == backButton){
@@ -77,14 +108,13 @@ public class BookingForm extends JFrame implements ActionListener, MouseListener
             System.out.println("support");
         }
         if(e.getSource() == bookButton){
-
+            bookNewFlight(displayFlight.get(this.currClickFlight));
         }
     }
 
     public void bookNewFlight(Flight newFlight){
         this.currUser.bookFlight(newFlight) ;
-        this.currUser.addFlierMiles(newFlight.getFlightMileage()) ;
-
+        this.currUser.addFlierMiles(Integer.parseInt(newFlight.getFlightMileage())) ;
         // send new flight to user's flights in the database
 
     }
@@ -101,13 +131,26 @@ public class BookingForm extends JFrame implements ActionListener, MouseListener
         this.scrollFlight.getViewport().add(flightsTable);
     }
 
+    // generate 2d array to return for populating the JTable for FlightsForm
+    public String[][] displayFlightData(ArrayList<Flight> displayFlight){
+        String[][] returnFlightData = new String[displayFlight.size()][6] ;
+        if(!displayFlight.isEmpty()){
+            for(int i = 0; i < displayFlight.size(); i++){
+                returnFlightData[i] = displayFlight.get(i).getFlightInformation() ;
+            }
+            return returnFlightData ;
+        }
+        return new String[0][0] ;
+    }
+
     //
     // override mouselistener methods
     //
     @Override
     public void mouseClicked(MouseEvent e) {
         int row = this.flightsTable.rowAtPoint(e.getPoint()) ;
-        Flight clickedFlight = currUser.getUserFlights().get(row) ;
+        Flight clickedFlight = displayFlight.get(row) ;
+        this.currClickFlight = row ;
         this.departDestLabel.setText(clickedFlight.getDepartDest()) ;
         this.arriveDestLabel.setText(clickedFlight.getArrivalDest()) ;
         this.departTimeLabel.setText(clickedFlight.getDepartTime()) ;
